a541b6f2a0a9282e25419a01c0cd770ca0d960c1
qianwu2 <qianwu2@ap.xom.com>
2020-07-13 21:43:44 +0800

    album [qian] add userinfo

--- src/UserInfo/UserInfo.js
+++ src/UserInfo/UserInfo.js
@@ -1,9 +1,30 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import './UserInfo.scss';
 
 class UserInfo extends Component {
   render() {
-    return <aside className="UserInfo">User Info</aside>;
+    return (
+        <div className="userInfo">
+          <h2>Leanne&nbsp;Graham</h2>
+          <span className="contact">Contact&nbsp;Me@</span>
+          <label className='email'>
+            <ul>Email</ul>
+            <ul>Sincere@april.biz</ul>
+          </label>
+          <label className='phone'>
+            <ul>Phone</ul>
+            <ul>1-770-736-8031</ul>
+          </label>
+          <label className='website'>
+            <ul>Website</ul>
+            <ul>hildegard.org</ul>
+          </label>
+          <label className='website'>
+            <ul>Company</ul>
+            <ul>Romaguera-Crona</ul>
+          </label>
+        </div>
+    );
   }
 }
 
